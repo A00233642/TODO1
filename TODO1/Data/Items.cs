53d7869116a6e197d7a6229cf64e97ba3a471da4
A00233642 <84159018+A00233642@users.noreply.github.com>
2021-07-19 00:09:07 -0400

    To-Do Class

---
+++ TODO1/Data/Items.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TODO1.Data
+{
+    public class Items
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(128, MinimumLength = 1)]
+        public string Name { get; set; }
+
+        public bool Done { get; set; }
+
+        public DateTime RegisterDate { get; set; } = DateTime.Now;
+
+    }
+}
